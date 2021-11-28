0ff2bfdad3d92a49b009abfcf19ef2986683be3a
Farhan Ahmed <mfarhanahmed40@gmail.com>
2021-11-29 01:29:21 +0500

    Updated Home, Login & Signup Page with firebase Authentication

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { auth } from '../../Components/configuration'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        auth.onAuthStateChanged(() => {
+            auth.signOut();
+            navigate("/login");
+        })
+    }
+
+    const CurrentUser = () => {
+        const [user, loading, error] = useAuthState(auth);
+
+        if (loading) {
+            return (
+                <div>
+                    <p>Loading...</p>
+                </div>
+            );
+        }
+        if (error) {
+            return (
+                <div>
+                    <p>Error: {error}</p>
+                </div>
+            );
+        }
+        if (user) {
+            return (
+                <div>
+                    <h1>{user.displayName}</h1>
+                    <button onClick={handleLogout}>Log out</button>
+                </div>
+            );
+        }
+        if (!auth.currentUser) {
+            navigate("/login");
+        }
+        return (
+            <>
+            </>
+        )
+    };
     return (
         <div>
-            Home
+            <h1>Home</h1>
+            <CurrentUser />
         </div>
     )
 }
